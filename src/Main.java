244ff1d887d621dab15f3c9b5532b7d11b977e23
ErickSalmoran7 <erick.salmoran1810@uppuebla.edu.mx>
2024-05-14 12:05:26 -0600

    COLECCIONES CON FUNCIONES LANDA VERSION3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.println( "hola Uppue");
+//        DatosPersonales obj = new DatosPersonales("Santiago","Izquierdo Varela");
+//        System.out.println(obj.getNombre());
+
+        Coleccion objc = new Coleccion();
+
+        DatosPersonales dp1 = new DatosPersonales("Santiago","Izquierdo Varela");
+        objc.add(new DatosPersonales("Erick","Salmoran Aca"));
+        objc.add(new DatosPersonales("Dereck","Diaz Hernandez"));
+        objc.add(new DatosPersonales("Javier","Sanchez Varela"));
+        objc.add(new DatosPersonales("Brenda","Corona Salmoran"));
+
+//        objc.show();
+//        objc.show2();
+        objc.buscar("Brenda");
+
     }
 }
